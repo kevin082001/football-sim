549c35e98c60170c01f15036654650c49515104c
pva-kkerzner <k.kerzner2001+pva@gmail.com>
2023-07-20 16:19:00 +0200

    Added 3rd tier league (Sweden)

--- src/enums/League.java
+++ src/enums/League.java
@@ -7,29 +7,45 @@ public enum League {
     BUNDESLIGA("Deutsche Bundesliga", Country.DE),
     BUNDESLIGA_2("Zweite Bundesliga", Country.DE),
     DRITTE_LIGA("3. Liga", Country.DE),
+
     BUNDESLIGA_AUT("Österreichische Bundesliga", Country.AT),
     AUT_LIGA_2("Zweite Liga", Country.AT),
+
     SERIE_A("Serie A", Country.IT),
     SERIE_B("Serie B", Country.IT),
+
     LALIGA("La Liga Santander", Country.ES),
     LALIGA_2("La Liga Smartbank", Country.ES),
+
     LIGUE1("Ligue 1", Country.FR),
     LIGUE2("Ligue 2", Country.FR),
+
     EPL("Premier League", Country.GB),
     EFL("EFL Championship", Country.GB),
     LEAGUE_ONE("League One", Country.GB),
     LEAGUE_TWO("League Two", Country.GB),
+
     BESTADEILD("Besta deild", Country.IS),
+
     BETRIDEILD("Betri deild", Country.FO),
+
     SUPERLIGA("Danish Superliga", Country.DK),
-    DEN_2("1.Division Denmark", Country.DK),
+    DEN_2("1.Division", Country.DK),
+    DEN_3("2.Division", Country.DK),
+
     ELITESERIEN("Eliteserien", Country.NO),
     OBOS_LIGAEN("OBOS-ligaen", Country.NO),
+
     ALLSVENSKAN("Allsvenskan", Country.SE),
     SUPERETTAN("Superettan", Country.SE),
+    ETTAN_NORRA("Ettan Norra", Country.SE),
+
     FINNLIIGA("Finnliiga", Country.FI),
+
     RAIFFEISEN("Raiffeisen Super League", Country.CH),
+
     MEISTRILIIGA("Meistriliiga", Country.EE),
+
     MLS("Major League Soccer", Country.US),
     USL("USL Championship", Country.US);
 
