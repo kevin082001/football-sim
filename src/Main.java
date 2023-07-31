b3558fb96c687a62a910b0db42a563d68f0f8f84
pva-kkerzner <k.kerzner2001+pva@gmail.com>
2023-07-31 16:50:49 +0200

    Reworked menu: Asking for country, then leagues in that country (instead of immediately asking for a start league)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import enums.Club;
+import enums.Country;
 import enums.League;
 import helper.ClubHelper;
 import helper.LeagueHelper;
@@ -12,8 +13,12 @@ public class Main { //TODO implement: transfer market, international cups (EL,CL
     private final Scanner sc = new Scanner(System.in);
     private final Random rand = new Random(System.nanoTime());
 
-    private Map<League, List<Club>> leagues = LeagueHelper.initLeagues();
+    private Map<Country, List<League>> countriesWithLeagues = LeagueHelper.initCountriesThatHaveLeagues();
+
+    //private Map<League, List<Club>> leagues = LeagueHelper.initLeagues();
+    private Map<League, List<Club>> playableLeagues = LeagueHelper.initPlayableLeagues();
     private Map<Club, List<Player>> clubs = ClubHelper.initClubs();
+
     private Club clubToManage;
 
     public static void main(String[] args) throws InterruptedException {
@@ -53,13 +58,17 @@ public class Main { //TODO implement: transfer market, international cups (EL,CL
 
     private League askStartLeague() {
         int i = 0;
-        List<League> leaguesList = leagues.keySet().stream().toList();
+
+        Country startCountry = askStartCountry();
+        List<League> leaguesList = League.BUNDESLIGA.getByCountry(startCountry);
+        //List<League> leaguesList = playableLeagues.keySet().stream().toList();
 
         System.out.println("----------------------------------------");
         System.out.println("- - - - -  SELECT YOUR LEAGUE  - - - - -");
         System.out.println("----------------------------------------");
         for (League l : leaguesList) {
-            System.out.println("(" + i + ") " + l.getName() + " (" + l.getCountry() + ")");
+            //System.out.println("(" + i + ") " + l.getName() + " (" + l.getCountry() + ")");
+            System.out.println("(" + i + ") " + l.getName());
             i++;
         }
         System.out.print(">> ");
@@ -69,5 +78,26 @@ public class Main { //TODO implement: transfer market, international cups (EL,CL
             askStartLeague();
         }
         return leaguesList.get(choice);
+    }
+
+    private Country askStartCountry(){
+        int i = 0;
+
+        List<Country> countryList = countriesWithLeagues.keySet().stream().toList();
+
+        System.out.println("----------------------------------------");
+        System.out.println("- - - - -  SELECT YOUR COUNTRY  - - - - -");
+        System.out.println("----------------------------------------");
+        for (Country c : countryList) {
+            System.out.println("(" + i + ") " + c.getName() + " (" + countriesWithLeagues.get(c).size() + " leagues)");
+            i++;
+        }
+        System.out.print(">> ");
+        int choice = sc.nextInt();
+        if (choice >= countryList.size() || choice < 0) {
+            System.out.println("Invalid option");
+            askStartCountry();
+        }
+        return countryList.get(choice);
     }
 }
