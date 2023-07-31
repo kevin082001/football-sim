8c0d427465a7c6fc0769ca9385240a1bb0fee1de
pva-kkerzner <k.kerzner2001+pva@gmail.com>
2023-07-31 16:50:09 +0200

    Separated playable and non-playable leagues, added method to get countries which have leagues

--- src/enums/League.java
+++ src/enums/League.java
@@ -4,66 +4,70 @@ import java.util.ArrayList;
 import java.util.List;
 
 public enum League {
-    BUNDESLIGA("Deutsche Bundesliga", Country.DE),
-    BUNDESLIGA_2("Zweite Bundesliga", Country.DE),
-    DRITTE_LIGA("3. Liga", Country.DE),
+    BUNDESLIGA("Deutsche Bundesliga", Country.DE, true),
+    BUNDESLIGA_2("Zweite Bundesliga", Country.DE, true),
+    DRITTE_LIGA("3. Liga", Country.DE, true),
 
-    BUNDESLIGA_AUT("Österreichische Bundesliga", Country.AT),
-    AUT_LIGA_2("Zweite Liga", Country.AT),
+    BUNDESLIGA_AUT("Österreichische Bundesliga", Country.AT, true),
+    AUT_LIGA_2("Zweite Liga", Country.AT, true),
 
-    SERIE_A("Serie A", Country.IT),
-    SERIE_B("Serie B", Country.IT),
+    SERIE_A("Serie A", Country.IT, true),
+    SERIE_B("Serie B", Country.IT, true),
 
-    LALIGA("La Liga Santander", Country.ES),
-    LALIGA_2("La Liga Smartbank", Country.ES),
+    LALIGA("La Liga Santander", Country.ES, true),
+    LALIGA_2("La Liga Smartbank", Country.ES, true),
 
-    LIGUE1("Ligue 1", Country.FR),
-    LIGUE2("Ligue 2", Country.FR),
+    LIGUE1("Ligue 1", Country.FR, true),
+    LIGUE2("Ligue 2", Country.FR, true),
 
-    EPL("Premier League", Country.GB),
-    EFL("EFL Championship", Country.GB),
-    LEAGUE_ONE("League One", Country.GB),
-    LEAGUE_TWO("League Two", Country.GB),
+    EPL("Premier League", Country.GB, true),
+    EFL("EFL Championship", Country.GB, true),
+    LEAGUE_ONE("League One", Country.GB, true),
+    LEAGUE_TWO("League Two", Country.GB, true),
 
-    BESTADEILD("Besta deild", Country.IS),
+    BESTADEILD("Besta deild", Country.IS, true),
 
-    BETRIDEILD("Betri deild", Country.FO),
+    BETRIDEILD("Betri deild", Country.FO, true),
 
-    SUPERLIGA("Danish Superliga", Country.DK),
-    DEN_2("1.Division", Country.DK),
-    DEN_3("2.Division", Country.DK),
+    SUPERLIGA("Danish Superliga", Country.DK, true),
+    DEN_2("1.Division", Country.DK, true),
+    DEN_3("2.Division", Country.DK, false),
 
-    ELITESERIEN("Eliteserien", Country.NO),
-    OBOS_LIGAEN("OBOS-ligaen", Country.NO),
-    POSTNORD_LIGAEN("PostNord-ligaen", Country.NO),
+    ELITESERIEN("Eliteserien", Country.NO, true),
+    OBOS_LIGAEN("OBOS-ligaen", Country.NO, true),
+    POSTNORD_LIGAEN("PostNord-ligaen", Country.NO, false),
 
-    ALLSVENSKAN("Allsvenskan", Country.SE),
-    SUPERETTAN("Superettan", Country.SE),
-    ETTAN_NORRA("Ettan Norra", Country.SE),
+    ALLSVENSKAN("Allsvenskan", Country.SE, true),
+    SUPERETTAN("Superettan", Country.SE, true),
+    ETTAN_NORRA("Ettan Norra", Country.SE, false),
 
-    DRUGA("Druga Liga", Country.SLO), //2nd tier
+    DRUGA("Druga Liga", Country.SLO, false), //2nd tier
 
-    B_NACIONAL("Primera B Nacional", Country.AR), //2nd tier
+    B_NACIONAL("Primera B Nacional", Country.AR, false), //2nd tier
 
-    YKKONEN("Ykkönen", Country.FI), //2nd tier
+    FINNLIIGA("Finnliiga", Country.FI, true),
 
-    PROFESIONAL("División Profesional", Country.BO),
+    YKKONEN("Ykkönen", Country.FI, false),
 
-    FINNLIIGA("Finnliiga", Country.FI),
+    PROFESIONAL("División Profesional", Country.BO, false),
 
-    RAIFFEISEN("Raiffeisen Super League", Country.CH),
+    RAIFFEISEN("Raiffeisen Super League", Country.CH, true),
 
-    MEISTRILIIGA("Meistriliiga", Country.EE),
+    MEISTRILIIGA("Meistriliiga", Country.EE, true),
 
-    MLS("Major League Soccer", Country.US),
-    USL("USL Championship", Country.US);
+    MLS("Major League Soccer", Country.US, true),
+    USL("USL Championship", Country.US, false);
 
     private final String name;
+
     private final Country country;
 
-    League(String name, Country country) {
+    private final boolean playable;
+
+    League(String name, Country country, boolean playable) {
         this.name = name;
         this.country = country;
+        this.playable = playable;
     }
 
     public List<League> getByCountry(Country country) {
@@ -76,6 +80,16 @@ public enum League {
             }
         }
         return result;
+    }
+
+    public List<League> getPlayableLeagues() {
+        List<League> leagues = new ArrayList<>();
+        for (League l : League.values()) {
+            if (l.playable) {
+                leagues.add(l);
+            }
+        }
+        return leagues;
     }
 
     public String getName() {
--- src/helper/LeagueHelper.java
+++ src/helper/LeagueHelper.java
@@ -1,6 +1,7 @@
 package helper;
 
 import enums.Club;
+import enums.Country;
 import enums.League;
 
 import java.util.HashMap;
@@ -16,4 +17,21 @@ public class LeagueHelper {
         return result;
     }
 
+    public static Map<League, List<Club>> initPlayableLeagues() {
+        Map<League, List<Club>> result = new HashMap<>();
+        for (League l : League.BUNDESLIGA.getPlayableLeagues()) {
+            result.put(l, ClubHelper.getClubsForLeague(l));
+        }
+        return result;
+    }
+
+    public static Map<Country, List<League>> initCountriesThatHaveLeagues() {
+        Map<Country, List<League>> result = new HashMap<>();
+        for (Country c : Country.values()) {
+            if (!League.BUNDESLIGA.getByCountry(c).isEmpty()) {
+                result.put(c, League.BUNDESLIGA.getByCountry(c));
+            }
+        }
+        return result;
+    }
 }
