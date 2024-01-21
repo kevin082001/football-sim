f0dfbbc138181ac1ec3388fe799de7a29cc8f765
pva-kkerzner <k.kerzner2001@gmail.com>
2024-01-21 21:30:46 +0100

    Added extend method for PlayerCareer arrays

--- src/helper/ArrayHelper.java
+++ src/helper/ArrayHelper.java
@@ -1,10 +1,19 @@
 package helper;
 
+import GameObjects.PlayerCareer;
 import enums.Club;
 
 public class ArrayHelper {
     public static Club[] extend(Club[] array) {
         Club[] newArray = new Club[array.length + 1];
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+        }
+        return newArray;
+    }
+
+    public static PlayerCareer[] extend(PlayerCareer[] array) {
+        PlayerCareer[] newArray = new PlayerCareer[array.length + 1];
         for (int i = 0; i < array.length; i++) {
             newArray[i] = array[i];
         }
